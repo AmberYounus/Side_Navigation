3b2610ae591313d5e68837a14026f177870ecce7
Amber Younus <amberyounus22@gmail.com>
2023-06-15 20:42:35 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+//open the navigation bar
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+
+//close the navigation bar
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  document.body.style.backgroundColor = "white";
+}
